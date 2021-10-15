4f9147064a3fd45daf16c918523e02506734ec0a
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-10-14 23:13:29 -0700

    initialize app

--- index.js
+++ index.js
@@ -3,13 +3,43 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: ,
+            name: ,
+            message: ,
+        },
+        {
+            type: ,
+            name: ,
+            message: ,
+        },
+        {
+            type: ,
+            name: ,
+            message: ,
+        },
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // TODO: Create a function to initialize app
 function init() {}
-
+  
 // Function call to initialize app
+const init = () => {
+    const init= inquirer.prompt({
+        type: "input",
+        message: "generate the README!",
+        name:"msg"
+    })
+    fs.appendFile ("inquirer.prompt",`${data.msg}`)
+    console.log("done!")
+};
+  
 init();
+
