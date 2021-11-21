2c8d79360a7972f8522fa3a1191f98b90826edbc
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-11-21 01:21:54 -0800

    markdown js

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-README generator
+# README generator
 
-Tech used: 
+## Tech used: 
 - npm
 - Node.js
 - inquire
 - modualarization
 
-Description: 
-Using Node.js to build a READ ME generator. Create a read me file by answering a series of questions using command line app. 
+## Description: 
+Using Node.js to build a README generator. Create a read me file by answering a series of questions using command line app. 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const badges = require (`./licenseBadges`)
+function generateMarkdown(data) {
+  return `# ${data.title}
+    
+${badges(data.licensing)}    
+    
+## Table of Contents 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [Additional Info](#additional-info)
+   
+## Description:
+${data.description}
+    
+## Installation:
+${data.installation}
+    
+## Usage:
+${data.usage}
+    
+    
+## Contribution:
+${data.contribution}
+   
+## Testing:
+${data.testing}
+
+## License:
+${data.licensing}
+
+## Additional Info:
+- Github profile: [${data.github}](https://github.com/${data.github})
+- Email me @ ${data.email} `;
+
+}
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
 module.exports = generateMarkdown;
